c6d06bb5bd25c60cbd14b3dd16e3b396b1573740
SaeedNikmehr <hovalkafi@gmail.com>
2021-01-02 00:50:46 +0330

    add .env file

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
 
-const mongoConnect = require('./config/database')
+const {mongoConnect , port} = require('./config/database')
 
 const app = express();
 
@@ -14,10 +14,9 @@ const app = express();
 
 
 
-const port = process.env.PORT || 5000
-mongoConnect(() => {
+mongoConnect().then(()=>{
     app.listen(port, () => {
         console.log(`listening on port ${port}`)
       })
-  });
+});
   
--- config/database.js
+++ config/database.js
@@ -1,25 +1,30 @@
 const { MongoClient } = require("mongodb")
 const Link = require("../app/models/Link")
+require('dotenv').config()
+
+const port = process.env.PORT || 5000
 
 
-
-
-const mongoConnect = (callback) => {
-    MongoClient.connect("mongodb+srv://admin:1234@cluster0.wjxzh.mongodb.net/littleLink?retryWrites=true&w=majority",
-    { useUnifiedTopology: true }
-    )
-    .then(async client => {
-        await Link.injectDB(client)
-        console.log('connected to database')
-        callback();
-        // app.listen(port, () => {
-        //     console.log(`listening on port ${port}`)
-        // })
-    })
-    .catch(err => {
-        console.error(err.stack)
-        process.exit(1)
-    })
-};
-
-module.exports = mongoConnect;
+const mongoConnect = async() => {
+    MongoClient.connect(
+        process.env.ATLAS_DB,
+        { useUnifiedTopology: true }
+        )
+        .then(async client => {
+            await Link.injectDB(client)
+            console.log('connected to database')
+            // callback();
+            // app.listen(port, () => {
+            //     console.log(`listening on port ${port}`)
+            // })
+        })
+        .catch(err => {
+            console.error(err.stack)
+            process.exit(1)
+        })
+    };
+    
+    module.exports = {
+        mongoConnect,
+        port
+    };
