a5dbed0ebe901ec9f0149b2961899d767c2b38ec
SaeedNikmehr <hovalkafi@gmail.com>
2021-02-11 03:03:56 +0330

    bug fix redis config

--- app/middlewares/cache/redis.js
+++ app/middlewares/cache/redis.js
@@ -1,8 +1,14 @@
 const Redis = require('ioredis')
 const { wsResponse } = require('../../providers/services/response')
 const {cache} = require('../../../config/database')
+const URL = require('url')
 
-const redis = new Redis(cache.redis.url);
+var redisUrl = URL.parse(cache.redis.url),redisAuth = redisUrl.auth.split(':');  
+const redis = new Redis({
+    port: redisUrl.port, // Redis port
+    host: redisUrl.hostname, // Redis host
+    password: redisAuth[1],
+  });
 
 const revertLinkCache=async (req, res, next)=>{
     const {shortLink} = req.body
