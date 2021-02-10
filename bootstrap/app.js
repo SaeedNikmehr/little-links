355b3c80873d1ebbc07d0c544c122f68a3d04e36
SaeedNikmehr <hovalkafi@gmail.com>
2021-02-11 00:16:14 +0330

    add procfile

--- bootstrap/app.js
+++ bootstrap/app.js
@@ -3,11 +3,13 @@
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const path = require('path')
+const helmet = require('helmet')
 
 module.exports = (app)=>{
     app.use(bodyParser.urlencoded({extended:false}))
     app.use(bodyParser.json())
     app.use(cors())
+    app.use(helmet())
     
 
 }
